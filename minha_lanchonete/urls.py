1b1ac8a2fc670be9203c849670161baf21776f08
ARTHUR PINHEIRO MACHADO <arthur.machado@centralit.com.br>
2025-01-06 18:20:28 -0300

    criando rotas

--- minha_lanchonete/urls.py
+++ minha_lanchonete/urls.py
@@ -1,37 +1,17 @@
-"""
-URL configuration for minha_lanchonete project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from rest_framework import routers
-from rest_framework_simplejwt.views import TokenRefreshView
 
-from users import views as UserViews
+from minha_lanchonete import views
 
 router = routers.DefaultRouter()
 
-router.register(r'admins', UserViews.AdminsView)
-
-urls = [
-    path('sign-in/', UserViews.LoginView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('sign-up/', UserViews.RegisterView.as_view(), name='auth_register'),
-]
+router.register(r'product/drink', views.DrinkView)
+router.register(r'order', views.OrderView)
+router.register(r'payment_method', views.PaymentMethodView)
+router.register(r'pizza', views.PizzaView)
+router.register(r'product', views.ProductView)
 
 urlpatterns = [
     path(r'api/', include(router.urls)),
-    path(r"auth/", include(urls))
+    path(r'auth/', include("users.urls"))
 ]
--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ from users import views
 
 
 urlpatterns = [
-    
+    path('sign-in/', views.LoginView.as_view(), name='token_obtain_pair'),
+    path('sign-up/', views.RegisterView.as_view(), name="auth_register")
 ]
